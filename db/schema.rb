7092d776f1324174a767e21bc07204c7c2ff54d2
Alexander Dubovoy <ajdubovoy@gmail.com>
2022-02-22 16:51:38 +0100

    make workspace model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_22_145815) do
+ActiveRecord::Schema.define(version: 2022_02_22_154758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,4 +27,14 @@ ActiveRecord::Schema.define(version: 2022_02_22_145815) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "workspaces", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_workspaces_on_user_id"
+  end
+
+  add_foreign_key "workspaces", "users"
 end
